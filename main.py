c17859df84a295ad3d01c0c5cd0739c11169a87c
Alexander Lyttle <a.j.lyttle@bham.ac.uk>
2025-05-01 11:55:34 +0100

    Add JAX profiling example script

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import jax
+import jax.numpy as jnp
+
+
 def main():
-    print("Hello from profile-jax!")
+    with jax.profiler.trace("/tmp/jax-trace", create_perfetto_link=True):
+        # Run the operations to be profiled
+        key = jax.random.key(0)
+        x = jax.random.normal(key, (5000, 5000))
+        y = x @ x
+        y.block_until_ready()
 
 
 if __name__ == "__main__":
