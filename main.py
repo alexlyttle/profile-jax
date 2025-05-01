d239dde42b90dc1fc70469ffa0b56df0083fd145
Alexander Lyttle <a.j.lyttle@bham.ac.uk>
2025-05-01 11:59:32 +0100

    Use TMPDIR environment variable if it exists

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+import pathlib
 import jax
-import jax.numpy as jnp
 
 
 def main():
-    with jax.profiler.trace("/tmp/jax-trace", create_perfetto_link=True):
+    log_dir = pathlib.Path(os.environ.get("TMPDIR", "/tmp")) / "jax-trace"
+    with jax.profiler.trace(log_dir, create_perfetto_link=True):
         # Run the operations to be profiled
         key = jax.random.key(0)
         x = jax.random.normal(key, (5000, 5000))
